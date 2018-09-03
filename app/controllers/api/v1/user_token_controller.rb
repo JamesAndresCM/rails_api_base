ec127410c023694911117ec15dd865388749c32c
JamesACM <jaimecanalesmarin@gmail.com>
2018-09-03 02:44:10 -0300

    cambio en respuesta json (login) alternativa de uso

--- app/controllers/api/v1/user_token_controller.rb
+++ app/controllers/api/v1/user_token_controller.rb
@@ -3,6 +3,18 @@ class Api::V1::UserTokenController < Knock::AuthTokenController
 
   rescue_from Knock.not_found_exception_class_name, with: :bad_request
 
+  #devuelve JWT mas propiedades de usuario al momento de logearse
+=begin
+  def create
+    @user = User.find_by(email: params[:auth][:email])
+    data = {username: @user.username, email: @user.email, role: @user.role}
+    render json: {
+        jwt: auth_token.token,
+        user: data,
+        status: 200
+      }
+  end
+=end
   # excepcion email-password al logearse
   def bad_request
     render json: { status: 401, msg: "Invalid email address or password" }
