b9cae1fd85918261086d548092410eb2ec69f899
JamesACM <jaimecanalesmarin@gmail.com>
2018-09-23 15:49:10 -0300

    include errors cancancan

--- app/controllers/api/v1/admin/dashboard_controller.rb
+++ app/controllers/api/v1/admin/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::Admin::DashboardController < ApplicationController
-  before_action :authenticate_user
+  include CancanWarning
   authorize_resource class: false
+  before_action :authenticate_user
 
   def index
     users = User.all
