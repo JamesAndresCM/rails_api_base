61c5bc46adb6ac07248fcbc4e1b119772d2ce01b
JamesACM <jaimecanalesmarin@gmail.com>
2018-09-23 16:46:32 -0300

    set root url & remove ver2 (only test)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Ui::Engine => '/api/v1/docs'
   mount Rswag::Api::Engine => '/api-docs'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   concern :api_base do
@@ -26,17 +26,16 @@ Rails.application.routes.draw do
       concerns :api_base
     end
   end
-#  versionamiento posible version 2 (crear namespace vN..) 
+#  versionamiento posible version 2 (ejemplo serializers y ruta extra) 
 #  https://chriskottom.com/blog/2017/04/versioning-a-rails-api/
-  namespace :api do
-    namespace :v2 do
-      concerns :api_base
-      get '/test' => 'home#test'
-    end
-  end
+#  namespace :api do
+#    namespace :v2 do
+#      concerns :api_base
+#      get '/test' => 'home#test'
+#    end
+#  end
     
-  #posible "/"
-  #root to: 'api/v1/home#index'
+  root :to => redirect('/api/v1/docs')
 
   #rutas no encontradas
   match '*unmatched_route', :to => 'api/v1/errors#routing', via: [:all]
