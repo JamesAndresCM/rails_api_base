4c93d719ef1e034140bf213f398767b2457fc7c2
JamesACM <jaimecanalesmarin@gmail.com>
2018-09-24 05:48:28 -0300

    dashboard only users

--- app/controllers/api/v1/admin/dashboard_controller.rb
+++ app/controllers/api/v1/admin/dashboard_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::Admin::DashboardController < ApplicationController
   before_action :authenticate_user
 
   def index
-    users = User.all
+    users = User.where.not(role: 0)
     render json: users
   end
 end
