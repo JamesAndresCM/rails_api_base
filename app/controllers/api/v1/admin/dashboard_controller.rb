638850b457b8964cafcc1d0235d9909ce2490d3a
JamesACM <jaimecanalesmarin@gmail.com>
2018-10-02 02:30:21 -0300

    remove pagination (TODO: btn front-end next...)

--- app/controllers/api/v1/admin/dashboard_controller.rb
+++ app/controllers/api/v1/admin/dashboard_controller.rb
@@ -4,7 +4,9 @@ class Api::V1::Admin::DashboardController < ApplicationController
   before_action :authenticate_user
 
   def index
-    users = User.where.not(role: 0).paginate(page: params[:page], per_page: 5).order(created_at: :desc)
+    #TODO: front-end button to next params[per_page]...
+    #users = User.where.not(role: 0).paginate(page: params[:page], per_page: 5).order(created_at: :desc)
+    users = User.where.not(role: 0).order(created_at: :desc)
     render json: users
   end
 end
